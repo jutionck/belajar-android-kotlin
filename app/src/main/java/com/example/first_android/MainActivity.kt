cb7cd933c184ac59a19342d511ca7b43a11a45dc
jutionck <jutionck@gmail.com>
2020-08-12 10:26:55 +0700

    Create counter app with fragmenent

--- app/src/main/java/com/example/first_android/MainActivity.kt
+++ app/src/main/java/com/example/first_android/MainActivity.kt
@@ -5,11 +5,23 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private val activityName = "MAIN_ACTIVITY"
+
+    var id = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val counterFragment = CounterFragment()
+//        val counterShowFragment = CounterShowFragment()
+//
+//        R.id.counterValue
+//
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.increaseButton, counterFragment)
+//            commit()
+//        }
     }
+
 
 }
